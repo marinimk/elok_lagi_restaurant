5585b59084720fa6d88ceb298e120e6f8efd463c
marini <marinimustafakamal@gmail.com>
2021-07-19 06:06:43 +0800

    umm the food cards are pretty now

--- lib/view/widgets/constants.dart
+++ lib/view/widgets/constants.dart
@@ -74,3 +74,8 @@ Row buttonTextRow(IconData icon, String text) {
     ],
   );
 }
+
+extension CapExtension on String {
+  String get inCaps => this.length > 0 ?'${this[0].toUpperCase()}${this.substring(1)}':'';
+  String get capitalizeFirstofEach => this.replaceAll(RegExp(' +'), ' ').split(" ").map((str) => str.inCaps).join(" ");
+}
