fcd3a1686c04e29c5b89bd3bf9e312632bfb68dd
marini <marinimustafakamal@gmail.com>
2021-08-07 23:05:53 +0800

    i think i'm finally done <3

--- lib/controller/auth.dart
+++ lib/controller/auth.dart
@@ -29,16 +29,17 @@ class AuthService {
   }
 
   // register with email and password
-  Future registerWithEmailAndPassword(String email, String password) async {
+  Future registerWithEmailAndPassword(String email, String password,
+      String name, String phoneNum, String location, String category) async {
     try {
       UserCredential result = await _auth.createUserWithEmailAndPassword(
           email: email, password: password);
       User user = result.user;
       await DatabaseService(uid: user.uid).updateRestaurantData(
-          'name',
-          'category',
-          'location',
-          'phoneNum',
+          name,
+          category,
+          location,
+          phoneNum,
           false,
           'https://firebasestorage.googleapis.com/v0/b/elok-lagi.appspot.com/o/dafultUser.png?alt=media&token=d817ae55-f30c-47c6-bd75-30348e18eb73');
       return _userFromFirebaseUser(user);
--- lib/view/authenticate/signinup.dart
+++ lib/view/authenticate/signinup.dart
@@ -11,13 +11,25 @@ class SignInUp extends StatefulWidget {
 
 class _SignInUpState extends State<SignInUp> {
   bool isSignupScreen = false;
-  bool isRememberMe = false;
 
-  String name = '';
+  String name = ''; 
   String phoneNum = '';
+  String location = '';
   String email = '';
   String password = '';
-  String cPassword = '';
+  String cpassword = '';
+  String category = '';
+
+  final List<String> categoryList = [
+    'Western Food',
+    'Malaysian Food',
+    'Fusion',
+    'Italian',
+    'Desserts',
+  ];
+
+  var passwordController = TextEditingController();
+  var cpasswordController = TextEditingController();
 
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   final AuthService _auth = AuthService();
@@ -51,11 +63,11 @@ class _SignInUpState extends State<SignInUp> {
           AnimatedPositioned(
             duration: Duration(milliseconds: 700),
             curve: Curves.easeInOutBack,
-            top: isSignupScreen ? 220 : 230,
+            top: isSignupScreen ? 100 : 230,
             child: AnimatedContainer(
               duration: Duration(milliseconds: 700),
               curve: Curves.easeInOutBack,
-              height: isSignupScreen ? 310 : 250,
+              height: isSignupScreen ? 600 : 250,
               padding: EdgeInsets.all(20),
               width: MediaQuery.of(context).size.width - 40,
               margin: EdgeInsets.symmetric(horizontal: 20),
@@ -165,11 +177,13 @@ class _SignInUpState extends State<SignInUp> {
           // mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            // buildTextField(Icons.house_siding, "Restaurant Name", false, false),
-            // buildTextField(Icons.phone_android, "Phone Number", false, false),
+            buildNameTextField(Icons.house_siding, "Restaurant Name"),
+            buildPhoneTextField(Icons.phone_android, "Phone Number"),
+            buildLocationTextField(Icons.phone_android, "Location"),
+            profileUpdateDropDown("Category"),
             buildEmailTextField(Icons.email, "Email"),
             buildPasswordTextField(Icons.lock, "Password"),
-            // buildTextField(Icons.lock, "Confirm Password", true, false),
+            buildCPasswordTextField(Icons.lock, "Confirm Password"),
             Container(
               width: 200,
               margin: EdgeInsets.only(top: 20),
@@ -199,7 +213,7 @@ class _SignInUpState extends State<SignInUp> {
     return AnimatedPositioned(
       duration: Duration(milliseconds: 700),
       curve: Curves.easeInOutBack,
-      top: isSignupScreen ? 490 : 430,
+      top: isSignupScreen ? 650 : 430,
       right: 0,
       left: 0,
       child: Center(
@@ -226,7 +240,7 @@ class _SignInUpState extends State<SignInUp> {
                       if (_formKey.currentState.validate()) {
                         setState(() => loading = true);
                         dynamic result = await _auth
-                            .registerWithEmailAndPassword(email, password);
+                            .registerWithEmailAndPassword(email, password, name, phoneNum, location, category);
                         if (result == null) {
                           setState(() {
                             loading = false;
@@ -280,13 +294,68 @@ class _SignInUpState extends State<SignInUp> {
     );
   }
 
+  Widget buildNameTextField(IconData icon, String hintText) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 8.0),
+      child: TextFormField(
+          onChanged: (val) => setState(() => name = val),
+          validator: RequiredValidator(errorText: 'Please enter your name'),
+          keyboardType: TextInputType.emailAddress,
+          decoration: textInputDecoration(icon, hintText)),
+    );
+  }
+
+  Widget buildLocationTextField(IconData icon, String hintText) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 8.0),
+      child: TextFormField(
+          onChanged: (val) => setState(() => location = val),
+          validator: RequiredValidator(errorText: 'Please enter your location'),
+          keyboardType: TextInputType.emailAddress,
+          decoration: textInputDecoration(icon, hintText)),
+    );
+  }
+
+  Widget buildPhoneTextField(IconData icon, String hintText) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 8.0),
+      child: TextFormField(
+          onChanged: (val) => setState(() => phoneNum = val),
+          validator: MultiValidator([
+            RequiredValidator(errorText: 'Please enter your phone number'),
+            MinLengthValidator(10,
+                errorText: "Please enter a valid phone number")
+          ]),
+          keyboardType: TextInputType.number,
+          decoration: textInputDecoration(icon, hintText)),
+    );
+  }
+
+  Padding profileUpdateDropDown(String initVal) {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(0, 2, 0, 8),
+      child: DropdownButtonFormField(
+        decoration:
+            textInputDecoration(Icons.category_outlined, null).copyWith(),
+        hint: Text(initVal),
+        items: categoryList.map((cat) {
+          return DropdownMenuItem(value: cat, child: Text(cat));
+        }).toList(),
+        validator: (val) {
+          String error;
+          if (val == null) error = 'Choose restaurant category';
+          return error;
+        },
+        onChanged: (val) => setState(() => category = val),
+      ),
+    );
+  }
+
   Widget buildEmailTextField(IconData icon, String hintText) {
     return Padding(
       padding: const EdgeInsets.only(bottom: 8.0),
       child: TextFormField(
-          onChanged: (val) {
-            setState(() => email = val);
-          },
+          onChanged: (val) => setState(() => email = val),
           validator: MultiValidator([
             RequiredValidator(errorText: 'Please enter an email'),
             EmailValidator(errorText: 'Email must be valid'),
@@ -300,14 +369,38 @@ class _SignInUpState extends State<SignInUp> {
     return Padding(
       padding: const EdgeInsets.only(bottom: 8.0),
       child: TextFormField(
-          onChanged: (val) {
-            setState(() => password = val);
+          onChanged: (val) => setState(() => password = val),
+          controller: passwordController,
+          validator: (val) {
+            String error;
+            if (val.isEmpty)
+              error = 'Please enter a password';
+            else if (val.length < 6)
+              error = 'Password must consist at least 6 characters';
+            return error;
           },
-          validator: MultiValidator([
-            RequiredValidator(errorText: 'Please enter a password'),
-            MinLengthValidator(6,
-                errorText: 'Password must consist at least 6 characters'),
-          ]),
+          obscureText: true,
+          keyboardType: TextInputType.text,
+          decoration: textInputDecoration(icon, hintText)),
+    );
+  }
+
+  Widget buildCPasswordTextField(IconData icon, String hintText) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 8.0),
+      child: TextFormField(
+          onChanged: (val) => setState(() => cpassword = val),
+          controller: cpasswordController,
+          validator: (val) {
+            String error;
+            if (val.isEmpty)
+              error = 'Please enter a password';
+            else if (val.length < 6)
+              error = 'Password must consist at least 6 characters';
+            else if (val != passwordController.text)
+              error = 'Password does not match';
+            return error;
+          },
           obscureText: true,
           keyboardType: TextInputType.text,
           decoration: textInputDecoration(icon, hintText)),
