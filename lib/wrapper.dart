a30d836f222020e68b3efac59e3043c3d3c2cfc6
marini <marinimustafakamal@gmail.com>
2021-08-10 23:28:13 +0800

    idk what changes i made

--- lib/wrapper.dart
+++ lib/wrapper.dart
@@ -1,13 +1,6 @@
-// import 'package:elok_lagi/master.dart';
-// import 'package:elok_lagi_restaurant/view/widgets/elrAppBar.dart';
 import 'package:elok_lagi_restaurant/models/users.dart';
-import 'package:elok_lagi_restaurant/view/authenticate/authenticate.dart';
-// import 'package:elok_lagi_restaurant/view/screen/profile/profile.dart';
-// import 'package:elok_lagi_restaurant/view/screen/order/order.dart';
-// import 'package:elok_lagi/screens/home.dart';
-// import 'package:elok_lagi_restaurant/view/authenticate/signinup.dart';
+import 'package:elok_lagi_restaurant/view/authenticate/signinup.dart';
 import 'package:elok_lagi_restaurant/view/screen/startup.dart';
-// import 'package:elok_lagi_restaurant/view/widgets/elrDrawer.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -18,7 +11,7 @@ class Wrapper extends StatelessWidget {
 
     // return either the Home or Authenticate widget
     if (user == null) {
-      return Authenticate();
+      return SignInUp();
     } else {
       return Startup();
     }
